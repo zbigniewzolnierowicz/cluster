7e3e608ff176ae00ac4b019dfd24d0257827bca3
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2023-10-01 22:27:49 +0200

    things

--- pulumi/.eslintrc.cjs
+++ pulumi/.eslintrc.cjs
@@ -1,13 +1,24 @@
 module.exports = {
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
   env: {
     es2021: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singlequote: true,
+        semi: true,
+      },
+    ],
+  },
   overrides: [
     {
       env: {
@@ -19,11 +30,4 @@ module.exports = {
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {},
 };
