e90ed70a17f00ad44784cfd9ecaf009b1567b816
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2023-10-03 22:01:18 +0200

    get rid of lerna

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,9 +1,15 @@
 import * as pulumi from "@pulumi/pulumi";
-import { proxmoxProvider } from "@zed-infra/proxmox";
+import * as proxmox from "@muhlba91/pulumi-proxmoxve";
 import { WireguardVirtualMachines } from "./wireguard";
 
 const config = new pulumi.Config("redo");
 const sshKey = config.getSecret("ssh-key");
+const proxmoxProvider = new proxmox.Provider('proxmox', {
+    username: config.getSecret("proxmox-username"),
+    password: config.getSecret("proxmox-password"),
+    insecure: true,
+    endpoint: config.get("proxmox-host")
+})
 
 if (!sshKey) throw new Error("ssh key is not set");
 
